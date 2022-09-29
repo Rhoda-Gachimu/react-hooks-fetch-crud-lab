d4e5c984e81716c9cdea83ca9481fd00b8327511
Rhoda Gachimu <gachimurhoda@gmail.com>
2022-09-29 09:57:16 +0300

    completed assignment

--- src/components/App.js
+++ src/components/App.js
@@ -5,11 +5,29 @@ import QuestionList from "./QuestionList";
 
 function App() {
   const [page, setPage] = useState("List");
+  const [questions, setQuestions] = useState([])
+
+  useEffect(() => {
+    fetch('http://localhost:4000/questions')
+      .then(result => result.json())
+      .then(data => {
+        setQuestions(data)
+      })
+  }, [])
+
+  function handleAddQuestion(newQuestion) {
+    setQuestions([...questions, newQuestion])
+  }
+
+  function handleDeleteQuestion(deletedQuestionId) {
+    setQuestions(questions.filter(question => question.id !== deletedQuestionId))
+  }
 
   return (
     <main>
       <AdminNavBar onChangePage={setPage} />
-      {page === "Form" ? <QuestionForm /> : <QuestionList />}
+      {page === "Form" ? <QuestionForm handleAddQuestion={handleAddQuestion} /> :
+      <QuestionList questions={questions} handleDeleteQuestion={handleDeleteQuestion} />}
     </main>
   );
 }
